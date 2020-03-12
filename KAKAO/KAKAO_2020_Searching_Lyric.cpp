cf9486d197149688bb42eb1f31b9a85f524c76bc
Jonny <jounhee0107@gmail.com>
2020-03-12 17:06:59 +0900

    Searching_Lyric with Trie struct

--- KAKAO/KAKAO_2020_Searching_Lyric.cpp
+++ KAKAO/KAKAO_2020_Searching_Lyric.cpp
@@ -2,41 +2,94 @@
 #include<vector>
 #include<map>
 #include<string>
-
 using namespace std;
-
-bool check(string word, string query) {
-	if (word.size() != query.size()) return false;
-	int w, q;
-	w = q = 0;
-
-	while (q < word.size() && w < word.size()) {
-		if (query[q] != word[w]) {
-			if (query[q] != '?') return false;
-		}
-		q++;
-		w++;
+struct Trie {
+	bool terminal;
+	Trie* next[26];
+	int cnt;
+	Trie() : terminal(false), cnt(1) {
+		memset(next, 0, sizeof(next));
 	}
-	return true;
-}
+	~Trie() {
+		for (int i = 0; i < 26; i++) if (next[i]) delete next[i];
+	}
+
+	void insert(const char* key) {
+		if (*key == '\0') terminal = true;
+		else {
+			int cur = *key - 'a';
+
+			if (next[cur] == NULL) next[cur] = new Trie();
+			else next[cur]->cnt++;
+
+			next[cur]->insert(key + 1);
+		}
+	}
+	int find(const char* key) {
+		int cur = *key - 'a';
+		if (*key == '?') {
+			int tmp = 0;
+			for (int k = 0; k < 26; k++) {
+				if (next[k] != NULL) tmp += next[k]->cnt;
+			}
+			return tmp;
+		}
+		if (next[cur] == NULL) return 0;
+		if (*(key + 1) == '?') return next[cur]->cnt;
+		return next[cur]->find(key + 1);
+
+	}
+
+};
+Trie* root[10001];
+Trie* reroot[10001];
+
 vector<int> solution(vector<string> words, vector<string> queries) {
-	vector<int> answer;
-	map<string, int> visit;
 
-	for (int q = 0; q < queries.size(); q++) {
-		string query = queries[q];
+	int wSize = words.size();
+	int qSize = queries.size();
 
-		if (visit[query] != 0) {
-			answer.push_back(visit[query]);
-			continue;
-		}
-		for (int w = 0; w < words.size(); w++) {
-			if (check(words[w], queries[q])) visit[query]++;
-		}
+	vector<int> answer(qSize, 0);
 
-		answer.push_back(visit[queries[q]]);
+	for (auto& w : words) {
+		int sz = w.size();
+
+		const char* c = w.c_str();
+
+		if (root[sz] == NULL) root[sz] = new Trie();
+		root[sz]->insert(c);
+
+		string reversed = w;
+		reverse(reversed.begin(), reversed.end());
+
+		const char* k = reversed.c_str();
+		if (reroot[sz] == NULL) reroot[sz] = new Trie();
+		reroot[sz]->insert(k);
+
 	}
 
+	int idx = 0;
+
+	for (auto& q : queries) {
+		int sz = q.size();
+
+		if (q[sz - 1] == '?') {
+			const char* c = q.c_str();
+
+			if (root[sz] == NULL) { idx++; continue; }
+			else answer[idx] = root[sz]->find(c);
+		}
+		else {
+			string re = q;
+			reverse(re.begin(), re.end());
+
+			const char* k = re.c_str();
+
+			if (reroot[sz] == NULL) { idx++; continue; }
+			else answer[idx] = reroot[sz]->find(k);
+		}
+		idx++;
+	}
 	return answer;
 }
 
@@ -45,14 +98,14 @@ int main() {
 	vector<string> queries;
 	words.push_back("frodo");
 	words.push_back("front");
-	words.push_back("frost"); 
-	words.push_back("frozen"); 
+	words.push_back("frost");
+	words.push_back("frozen");
 	words.push_back("frame");
 	words.push_back("kakao");
 
 	queries.push_back("fro??");
 	queries.push_back("????o");
-	queries.push_back("fr???"); 
+	queries.push_back("fr???");
 	queries.push_back("fro???");
 	queries.push_back("pro?");
 
