fb450dc31c8545eafb75cbc103e48134978e7c19
Jonny <jounhee0107@gmail.com>
2020-04-04 06:26:20 +0900

    2020-04-04

--- KAKAO/KAKAO_2019_WINTER_INTERN_CROSSTHEBRIDGE.cpp
+++ KAKAO/KAKAO_2019_WINTER_INTERN_CROSSTHEBRIDGE.cpp
@@ -7,37 +7,40 @@
 #include<ctime>
 
 using namespace std;
+int canGo(int left, int right, int m, vector<int>& stones) {
 
+	if (left == right) return (stones[left] - m) < 0;
 
-bool is_possible(int mid, const vector<int>& stones, int k)
-{
-	int now = -1;
-	for (int i = 0; i < stones.size(); i++)
-	{
-		if (stones[i] - mid + 1 > 0)
-		{
-			if (i - now > k)return false;
-			now = i;
-		}
-	}
-	if (stones.size() - now > k)return false;
-	else return true;
+	int mid = (left + right) / 2;
+
+	int ret = max(canGo(left, mid, m, stones), canGo(mid + 1, right, m, stones));
+
+	int lo = mid;
+	int hi = mid + 1;
+	int cnt = 0;
+	while (hi <= right && (stones[hi] - m) < 0) hi++, cnt++;
+	while (lo >= left && (stones[lo] - m) < 0) lo--, cnt++;
+	ret = max(ret, cnt);
+	return ret;
 }
-
 int solution(vector<int> stones, int k) {
 	int answer = 0;
+	int sz = stones.size();
+	int left = 0, right = 2e8;
 
-	int left = 0, right = (int)2e8;
-	while (left <= right)
-	{
-		int mid = (left + right) / 2;
-		if (is_possible(mid, stones, k))
-		{
-			answer = mid;
-			left = mid + 1;
+	while (left <= right) {
+		int m = (left + right) / 2;
+
+		int obj = canGo(0, sz - 1, m, stones);
+
+		if (obj < k) {
+			answer = m;
+			left = m + 1;
 		}
-		else right = mid - 1;
+		else right = m - 1;
 	}
+
+
 	return answer;
 }
 int main() {
